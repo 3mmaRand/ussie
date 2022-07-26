d3fe32214f72b9ade6a7f32c9863de8d7d689a6a
Emma Rand <emma.rand@york.ac.uk>
2022-07-26 12:16:09 -0400

    btt22::btt_reset_hard("2.2.2")

--- R/matches.R
+++ R/matches.R
@@ -1,27 +1,38 @@
-#' Makes a standard dataframe for league data
+#' Make a standard league-play tibble
 #'
-#' @param data_engsoc dataframe from engsoccerdata package
-#' @param country a county one of spain, italy, germany, england, holland
+#' Given a league-play data frame from {engsoccer}, returns a tibble with
+#' standardised colomn-names and types, e.g. `date` is a `Date`.
 #'
-#' @return dataframe, tibble
-#' @export
+#' @param data_engsoc `data.frame` obtained from {engsoccerdata}.
+#' @param country `character` scalar, specifies the league.
+#'   `uss_countries()` returns choices available from {engsoccerdata}
+#'
+#' @return [tibble][tibble::tibble-package] with columns `country`, `date`,
+#'  `season`, `tier`, `home`, `visitor`, `goals_home`, `goals_visitor`.
 #'
 #' @examples
+#' uss_make_matches(engsoccerdata::italy, "Italy")
+#' @keywords internal
+#' @export
 #'
-#' uss_make_matches(engsoccerdata::spain, "Spain")
 uss_make_matches <- function(data_engsoc, country) {
+  
+  # validate
+
+  # put into "standard" form
   result <-
     data_engsoc |>
     tibble::as_tibble() |>
     dplyr::transmute(
-      country = as.character(country),
-      tier = factor(tier, levels = c("1", "2", "3", "4")),
-      season = as.integer(Season),
-      date = as.Date(Date),
-      home = as.character(home),
-      visitor = as.character(visitor),
-      goals_home = as.integer(hgoal),
-      goals_visitor = as.integer(vgoal)
+      country = as.character(.env$country),
+      tier = factor(.data$tier, levels = c("1", "2", "3", "4")),
+      season = as.integer(.data$Season),
+      date = as.Date(.data$Date),
+      home = as.character(.data$home),
+      visitor = as.character(.data$visitor),
+      goals_home = as.integer(.data$hgoal),
+      goals_visitor = as.integer(.data$vgoal)
     )
+  
   result
 }
